1234c2843a803d260e17eaf51ac9793f2d009851
阿Cai <jimmyrss1102@gmail.com>
2024-11-05 11:32:42 +0800

    refactor: reset

--- README.md
+++
@@ -1,5 +0,0 @@
-# @vegetables-school/pnpm-monorepo-template
-
-[参考](https://pnpm.io/using-changesets)
-
-egoism `pnpm-monorepo-template`
--- docs/server/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import { customText } from "@vegetables-school/ui"
-
-export default eventHandler((event) => {
-  return customText();
-});
